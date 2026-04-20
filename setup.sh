380c092c6c457401b509b526b1a41a9624d4426e
nganbee <ngankimm510@gmail.com>
2026-04-20 15:51:38 +0700

    feat: add install package script

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "--- Start install package ---"
+
+pip install pip3-autoremove
+
+pip install torch torchvision torchaudio xformers --index-url https://download.pytorch.org/whl/cu128
+
+pip install "unsloth[colab-new] @ git+https://github.com/unslothai/unsloth.git"
+
+pip install transformers==4.56.2
+
+pip install --no-deps trl==0.22.2
+
+echo "--- Installed successfully ---"
